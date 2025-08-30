4f66a9c4db2db037e58254c0cf74958fb676bbd7
kosta <k.sovaridis@inotek.be>
2025-08-30 09:53:44 +0200

    pin changes for the board

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use cc_talk_device::payout_device::PayoutDevice;
 use defmt::{error, info};
 use embassy_executor::Spawner;
 use embassy_stm32::exti::ExtiInput;
-use embassy_stm32::gpio::{Level, Output, Pull, Speed};
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_stm32::usart::{Config as UartConfig, DataBits, Parity, StopBits, Uart};
 use embassy_stm32::{bind_interrupts, peripherals, usart, Config};
 use embedded_io_async::Write;
@@ -32,25 +32,23 @@ async fn main(spawner: Spawner) {
     let in_3_pin = Output::new(p.PA4, Level::Low, Speed::Low);
 
     // Feedback pins
-    let high_level_sensor = ExtiInput::new(p.PB1, p.EXTI1, Pull::Up);
-    let low_level_sensor = ExtiInput::new(p.PB11, p.EXTI11, Pull::Up);
-    let exit_sensor = ExtiInput::new(p.PB12, p.EXTI12, Pull::Up);
+    let high_level_sensor = ExtiInput::new(p.PA8, p.EXTI8, Pull::Up);
+    let low_level_sensor = ExtiInput::new(p.PB2, p.EXTI2, Pull::Up);
+    let exit_sensor = ExtiInput::new(p.PC6, p.EXTI6, Pull::Up);
 
     // Security output
-    let security_output = ExtiInput::new(p.PA2, p.EXTI2, Pull::Up);
-
-    let user_button = ExtiInput::new(p.PC13, p.EXTI13, Pull::Down);
+    let security_output = ExtiInput::new(p.PA9, p.EXTI9, Pull::Up);
 
     // Address dip switches
-    //let addr_1 = Input::new(p.PG6, Pull::None);
-    //let addr_2 = Input::new(p.PG5, Pull::None);
-    //let addr_3 = Input::new(p.PG8, Pull::None);
+    let addr_1 = Input::new(p.PB3, Pull::None);
+    let addr_2 = Input::new(p.PB4, Pull::None);
+    let addr_3 = Input::new(p.PB5, Pull::None);
 
-    //let address = compute_bus_address(addr_1.get_level(), addr_2.get_level(), addr_3.get_level());
-    set_bus_address(3).await;
+    let address = compute_bus_address(addr_1.get_level(), addr_2.get_level(), addr_3.get_level());
+    info!("Hopper address: {}", address);
+    set_bus_address(address).await;
 
     spawner.spawn(reset_task(in_1_pin, in_2_pin)).unwrap();
-    spawner.spawn(reset_hopper(user_button)).unwrap();
     init_payout_tasks(
         spawner,
         in_3_pin,
@@ -73,7 +71,7 @@ async fn main(spawner: Spawner) {
     };
     let mut uart = Uart::new_half_duplex(
         p.USART1,
-        p.PC4,
+        p.PB6,
         Irqs,
         p.DMA1_CH1,
         p.DMA1_CH2,
